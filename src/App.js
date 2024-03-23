89810af758dfbb07cd491bb38ba1eca4e40739fc
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-03-23 17:03:12 +0530

    firebase done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import { useEffect } from 'react';
 import './App.css';
 import bg from "./assets/video.mp4";
+import { db } from './config/firebase';
+import { collection} from 'firebase/firestore';
+import { onSnapshot } from "firebase/firestore";
 
 function App() {
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "images"), (snapshot) => {
+      const alldata = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      const newlist = alldata.map(item => item.link)
+      console.log(newlist.slice(0,4));
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+
   return (
     <div className='gridPage'>
-      <video height={"768px"} width={"1024px"} autoPlay muted loop className='video'>
+      <video autoPlay muted loop className='video'>
         <source src={bg} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className="main">
+
+      </div>
     </div>
   );
 }
